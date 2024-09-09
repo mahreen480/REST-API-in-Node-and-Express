f3e2f49363fb4f52bbfe85e1551f10479a2a4f30
mahreen480 <152952923+mahreen480@users.noreply.github.com>
2024-09-09 21:50:50 +0500

    web mobile done

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const express =  require("express");
+const users = require("./MOCK_DATA.json")
+
 
 const app = express()
 const PORT = 8000;
+
+
+//Route
+
+
+//This is json format in mobile app
+app.get("/api/users" , (req,res)=>{
+    return res.json(users)
+})
+
+
+//this is the html format in web app
+
+app.get("/users",(req,res)=>{
+   const html = `
+   <ul>
+        ${users.map((users) => `<li>${users.first_name}</li>`).join("")}
+   </ul>
+   `
+
+   res.send(html)
+})
+
 
 app.listen(PORT , ()=>console.log("Server Started"))
  
