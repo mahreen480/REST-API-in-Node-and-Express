fb2beac8ea17d0f8763882c4ad6516d9225ca670
mahreen480 <152952923+mahreen480@users.noreply.github.com>
2024-09-09 22:26:05 +0500

    add methods

--- index.js
+++ index.js
@@ -23,9 +23,44 @@ app.get("/users",(req,res)=>{
         ${users.map((users) => `<li>${users.first_name}</li>`).join("")}
    </ul>
    `
-
    res.send(html)
 })
+
+//Get users by id dynamically
+
+// app.get("/api/users/:id",(req,res)=>{
+
+//     const id = Number(req.params.id);
+//     const user = users.find((user) => user.id === id);
+//     return res.json(user);
+
+// })
+
+// app.post("/api/users",(req,res)=>{
+//     //TODO: CREATE new user
+//     return res.json({status :"Pending"})
+// })
+
+// app.patch("/api/users/:id",(req,res)=>{
+//     //TODO: EDIT the user with id
+// })
+
+// app.delete("/api/users/:id",(req,res)=>{
+//     //TODO: Delete the user with id
+// })
+
+
+app.route("/api/users/:id").get((req,res)=>{
+
+        const id = Number(req.params.id);
+        const user = users.find((user) => user.id === id);
+        return res.json(user);
+    
+    }).patch((req,res)=>{
+            //TODO: EDIT the user with id
+        }).delete((req,res)=>{
+                //TODO: Delete the user with id
+            })
 
 
 app.listen(PORT , ()=>console.log("Server Started"))
