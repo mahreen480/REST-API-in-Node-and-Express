b72f7a0994e444265c9717199776f2523cc087ee
mahreen480 <152952923+mahreen480@users.noreply.github.com>
2024-09-10 14:49:58 +0500

    change name

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express =  require("express");
+const express = require("express");
 const users = require("./MOCK_DATA.json")
 
 
-const app = express()
+const app1 = express()
 const PORT = 8000;
 
 
@@ -10,20 +10,20 @@ const PORT = 8000;
 
 
 //This is json format in mobile app
-app.get("/api/users" , (req,res)=>{
+app1.get("/api/users", (req, res) => {
     return res.json(users)
 })
 
 
 //this is the html format in web app
 
-app.get("/users",(req,res)=>{
-   const html = `
+app1.get("/users", (req, res) => {
+    const html = `
    <ul>
         ${users.map((users) => `<li>${users.first_name}</li>`).join("")}
    </ul>
    `
-   res.send(html)
+    res.send(html)
 })
 
 //Get users by id dynamically
@@ -50,18 +50,17 @@ app.get("/users",(req,res)=>{
 // })
 
 
-app.route("/api/users/:id").get((req,res)=>{
+app1.route("/api/users/:id").get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
 
-        const id = Number(req.params.id);
-        const user = users.find((user) => user.id === id);
-        return res.json(user);
-    
-    }).patch((req,res)=>{
-            //TODO: EDIT the user with id
-        }).delete((req,res)=>{
-                //TODO: Delete the user with id
-            })
+}).patch((req, res) => {
+    //TODO: EDIT the user with id
+}).delete((req, res) => {
+    //TODO: Delete the user with id
+})
 
 
-app.listen(PORT , ()=>console.log("Server Started"))
- 
+
+app1.listen(PORT, () => console.log("Server Started"))
