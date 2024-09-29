60df483fd2e0ecd0e708ce753be8c6f2c105b227
mahreen480 <152952923+mahreen480@users.noreply.github.com>
2024-09-29 13:06:53 +0500

    set all

--- index.js
+++ index.js
@@ -5,29 +5,27 @@ const { type } = require("os");
 const { strict } = require("assert");
 
 
-//import routers
-const userRouter = require("./routes/user")
-
-//connection
-const {connectMongoDB} = require("./connection");
-
-connectMongoDB("mongodb+srv://mahreenmughal480:Abc123@database.f12uo.mongodb.net/?retryWrites=true&w=majority&appName=Database",{dbName: "RESTAPI"})
+//import connection
+const { connectMongoDB } = require("./connection");
 
 //import middlewares
 const { logReqRes } = require("./middleware")
 
+//import routers
+const userRouter = require("./routes/user")
+
 const app = express()
 const PORT = 8000;
 
-app.use(express.urlencoded({ extended: false }))
+//connection
+connectMongoDB("mongodb+srv://mahreenmughal480:Abc123@database.f12uo.mongodb.net/?retryWrites=true&w=majority&appName=Database",{dbName: "RESTAPI"})
 
+//middleware - Plugins
+app.use(express.urlencoded({ extended: false }))
 app.use(logReqRes("log.txt"))
 
-
-
-
-//Route
+//Routes
 app.use("/user", userRouter)
 
-
+//Listen on PORT
 app.listen(PORT, () => console.log("Server Started"))
